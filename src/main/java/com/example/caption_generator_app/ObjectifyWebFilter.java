4c1421e36af7c82df592a8d97a4edf4bf574e5ce
Ayushi Garg <91152845+ayushigarg01@users.noreply.github.com>
2022-05-03 02:49:54 -0700

    Update ObjectifyWebFilter.java

--- src/main/java/com/example/caption_generator_app/ObjectifyWebFilter.java
+++ src/main/java/com/example/caption_generator_app/ObjectifyWebFilter.java
@@ -8,5 +8,6 @@ import com.googlecode.objectify.ObjectifyFilter;
  * Filter required by Objectify to clean up any thread-local transaction contexts and pending
  * asynchronous operations that remain at the end of a request.
  */
+
 @WebFilter(urlPatterns = {"/*"})
 public class ObjectifyWebFilter extends ObjectifyFilter {}
